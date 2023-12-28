44fd2c66b0d351c1144d99370af2936d0e497369
Ayoub <ayoub.fiadh@gmail.com>
2023-12-28 18:11:32 -0500

    Init products

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
-<h1>
-	hello world
-</h1>
+<script>
+  import ProductComponent from "$lib/components/ProductComponent.svelte";
+
+	let products = [];
+</script>
+<div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+	<div class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+		{#if products?.length > 0}
+			{#each products as product}
+				<ProductComponent {product} />
+			{/each}
+		{/if}
+	</div>
+</div>

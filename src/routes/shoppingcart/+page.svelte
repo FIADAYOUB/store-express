58013db942e49bf13cff7313cc81abdb3fa31b20
Ayoub <ayoub.fiadh@gmail.com>
2023-12-29 11:28:34 -0500

    Create shoppingcart page

---
+++ src/routes/shoppingcart/+page.svelte
@@ -0,0 +1,118 @@
+<script>
+  import visa from "$lib/images/visa.png";
+  import mastercard from "$lib/images/mastercard.png";
+  import paypal from "$lib/images/paypal.png";
+  import applepay from "$lib/images/applepay.png";
+  import cartempty from "$lib/images/cart-empty.png";
+  import CartItem from "./CartItem.svelte";
+
+  const cards = [visa, mastercard, paypal, applepay];
+
+  let user;
+  let selectedArray = [];
+
+  let cart = [
+    {
+      id: 1,
+      title: "Product 1",
+      description: "This is description",
+      url: "https://picsum.photos/id/1/800/800",
+      price: 999,
+    },
+    {
+      id: 2,
+      title: "Product 2",
+      description: "This is description",
+      url: "https://picsum.photos/id/3/800/800",
+      price: 80,
+    },
+  ];
+
+  let totalPriceComputed = 0;
+
+  function selectedRadioFunc() {}
+
+  function goToCheckout() {}
+</script>
+
+<div id="ShoppingCartPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+  {#if !cart.length}
+    <div class="h-[500px] flex items-center justify-center">
+      <div class="pt-20">
+        <img class="mx-auto w-[250px]" src={cartempty} />
+
+        <div class="text-xl text-center mt-4">No items yet?</div>
+        {#if !user}
+          <div class="flex text-center">
+            <a
+              href="/auth"
+              class="bg-[#FD374F] w-full text-white text-[21px] font-semibold p-1.5 rounded-full mt-4"
+            >
+              Sign in
+            </a>
+          </div>
+        {/if}
+      </div>
+    </div>
+  {:else}
+    <div class="md:flex gap-4 justify-between mx-auto w-full">
+      <div class="md:w-[65%]">
+        <div class="bg-white rounded-lg p-4">
+          <div class="text-2xl font-bold mb-2">
+            Shopping Cart ({cart.length})
+          </div>
+        </div>
+
+        <div class="bg-[#FEEEEF] rounded-lg p-4 mt-4">
+          <div class="text-red-500 font-bold">
+            Welcome Deal applicable on 1 item only
+          </div>
+        </div>
+
+        <div id="Items" class="bg-white rounded-lg p-4 mt-4">
+          {#each cart as product}
+            <CartItem
+              {product}
+            />
+          {/each}
+        </div>
+      </div>
+
+      <div class="md:hidden block my-4" />
+      <div class="md:w-[35%]">
+        <div id="Summary" class="bg-white rounded-lg p-4">
+          <div class="text-2xl font-extrabold mb-2">Summary</div>
+          <div class="flex items-center justify-between my-4">
+            <div class="font-semibold">Total</div>
+            <div class="text-2xl font-semibold">
+              $ <span class="font-extrabold">{totalPriceComputed}</span>
+            </div>
+          </div>
+          <button
+            on:click={goToCheckout}
+            class=" flex items-center justify-center bg-[#FD374F] w-full text-white text-[21px] font-semibold p-1.5 rounded-full mt-4"
+          >
+            Checkout
+          </button>
+        </div>
+
+        <div id="PaymentProtection" class="bg-white rounded-lg p-4 mt-4">
+          <div class="text-lg font-semibold mb-2">Payment methods</div>
+          <div class="flex items-center justify-start gap-8 my-4">
+            {#each cards as card}
+              <img class="h-6" src={card} alt="" />
+            {/each}
+          </div>
+
+          <div class="border-b" />
+
+          <div class="text-lg font-semibold mb-2 mt-2">Buyer Protection</div>
+          <p class="my-2">
+            Get full refund if the item is not as described or if is not
+            delivered
+          </p>
+        </div>
+      </div>
+    </div>
+  {/if}
+</div>
---
+++ src/routes/shoppingcart/CartItem.svelte
@@ -0,0 +1,49 @@
+<script>
+  import Icon from "@iconify/svelte";
+
+  export let product;
+
+  function removeFromCart() {}
+</script>
+
+<div class="flex justify-start my-2">
+
+  <img class="rounded-md md:w-[150px] w-[90px]" src={product.url} alt=""/>
+
+  <div class="overflow-hidden pl-2 w-full">
+    <div class="flex items-center justify-between w-full">
+      <div class="flex items-center justify-between truncate">
+        <span
+          class="sm:block hidden bg-[#FD374F] text-white text-[9px] font-semibold px-1.5 rounded-sm min-w-[80px]"
+          >Welcome Deal</span
+        >
+        <div class="truncate sm:pl-2">{product.title}</div>
+      </div>
+      <button
+        on:click={removeFromCart}
+        class="mx-3 sm:block hidden -mt-0.5 hover:text-red-500"
+      >
+        <Icon icon="material-symbols:delete-outline" />
+      </button>
+    </div>
+
+    <div class="text-xl font-semibold">
+      $ <span class="font-bold">{product.price}</span>
+    </div>
+
+    <p class="text-[#009A66] text-xs font-semibold pt-1">
+      Free 11-day delivery over ￡8.28
+    </p>
+
+    <p class="text-[#009A66] text-xs font-semibold pt-1">Free Shipping</p>
+
+    <div class="flex items-center justify-end">
+      <button
+        on:click={removeFromCart}
+        class="sm:hidden block -mt-0.5 hover:text-red-500"
+      >
+        <Icon icon="material-symbols:delete-outline" />
+      </button>
+    </div>
+  </div>
+</div>

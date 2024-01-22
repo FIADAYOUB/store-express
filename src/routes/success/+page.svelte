7ff2dd17cb900bad5354238d8fd2894852366590
Ayoub <ayoub.fiadh@gmail.com>
2024-01-21 20:45:12 -0500

    Add success page

---
+++ src/routes/success/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+
+  import Icon from "@iconify/svelte";
+
+</script>
+<div class="mt-4 max-w-[1200px] mx-auto px-2">
+  <div class="bg-white w-full p-6">
+    <div class="flex items-center text-xl">
+      <Icon icon="clarity:success-standard-line" color="#5FCB04" />
+      <span class="pl-4">Payment Successful</span>
+    </div>
+    <p class="text-sm pl-[50px]">Thank you! We've received your payment.</p>
+  </div>
+</div>
